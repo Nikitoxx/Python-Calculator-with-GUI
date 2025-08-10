69e3c8881f10fde0dc6ad268c173d455b5ac27bb
Nikitoxx <nekithis@gmail.com>
2025-08-10 14:33:28 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import * 
+
+root =Tk()
+root.title('Calculator')
+root.config(bg="black")
+root.resizable(False, False)
+
+
+
+
+
+buttons = [['7','8','9','+'],
+           ['4','5','6','-'],
+           ['1','2','3','*'],
+           ['.','0','/','=']
+]
+
+for i in range(len(buttons)):
+    for j in range(len(buttons)):
+        Button(text=buttons[i][j], 
+               bg='orange',
+               fg='black',
+               font='Arial, 15',
+               width=5,
+               height=2).grid(column=j, row=i)
+Entry().get()
+        
+
+root.mainloop()
