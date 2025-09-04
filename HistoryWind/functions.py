e79013e1a94da5f4df330e32fe389bbf362a322c
Nikita Raylean <nekithis@gmail.com>
2025-09-04 21:29:11 +0300

    Added the history

--- HistoryWind/functions.py
+++ HistoryWind/functions.py
@@ -1,24 +1,22 @@
 from tkinter import *
 
 
-def create_button(column, row, text, command):
-    Button(text=text, 
-               bg='orange',
-               fg='white',
-               font=('Arial', 15),
-               width=5,
-               height=2,
-               command=command).grid(column=column, row=row, 
-                              padx=1, pady=1,
-                              sticky='wens')
-
-
 def create_new_window():
-    #New window with history of operations
+    
+    '''New window with history of operations'''
     
     wnd = Tk()
     wnd.title('Calculator history')
     wnd.config(bg="black")
     wnd.resizable(False, False)
     wnd.geometry()
+    
+    with open('history.txt', 'r') as file:
+        a = file.readlines()
+        for i, v in enumerate(a, 1):
+            Label(wnd, text=f"{i}. {v}", fg='white', bg='black', font='Arial, 15', anchor='w', justify='left').pack(side='top', fill='x')
+        
+        
+    
+    
     
